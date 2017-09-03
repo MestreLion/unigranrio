003a824d4301c00c8e5b0df00ac2727e015794b7
MestreLion <linux@rodrigosilva.com>
2017-09-03 15:02:37 -0300

    [ARQ] TDE03: add question 2 prototype

---
+++ Arquitetura de Software/tde03/tde03_2.java
@@ -0,0 +1,88 @@
+/*
+ * TDE03-2 - Encontra uma string num array de strings, e informa sua posição
+ *
+ *   Copyright (C) 2017 Rodrigo Silva (MestreLion) <linux@rodrigosilva.com>
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program. See <http://www.gnu.org/licenses/gpl.html>
+ */
+
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class tde03_2
+{
+	public static void main(String[] args)
+	{
+		ArrayList<String> linhas;
+
+		if(args.length != 2) {
+			usage();
+			System.exit(1);
+		}
+
+		linhas = linearray(args[0]);
+		System.out.println(linhas.size());
+	}
+
+
+	private static void usage(String msg, Throwable e)
+	{
+		String err = "";
+		String out = "";
+
+		if (e != null)
+			err = e.getMessage();
+
+		if(notBlank(msg) && notBlank(err))
+			out = String.format("%s: %s", msg, err);
+		else
+			out = String.format("%s%s", msg, err);
+
+		System.err.println("TDE03-2: Encontra a linha de uma string em um arquivo texto");
+		if(notBlank(out))
+			System.err.println("ERRO: " + out);
+		System.err.println("Uso: `java tde03_2 ARQUIVO STRING`");
+	}
+	private static void usage()            { usage("",  null); }
+	private static void usage(String msg)  { usage(msg, null); }
+	private static void usage(Throwable e) { usage("",  e); }
+
+
+	private static boolean notBlank(String s)
+	{
+		return (s != null && s.length() == 0 && s.trim().length() == 0);
+	}
+
+
+	private static ArrayList<String> linearray(String path)
+	{
+		ArrayList<String> out = new ArrayList<String>();
+		BufferedReader buf = null;
+		try {
+			buf = new BufferedReader(new FileReader(path));
+			for (String line; (line = buf.readLine()) != null;)
+				out.add(line);
+		}
+		catch (IOException e) {
+			usage("Não foi possível ler o arquivo", e);
+		}
+		finally {
+			if (buf != null)
+				try { buf.close(); } catch (IOException e) {}
+		}
+		return out;
+	}
+}
